2b6b97eb218fae051d15c019982f54f2035abb8d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-24 22:11:16 +0530

    feat: initiate ticket web components

---
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/web/ticket/DTOs/TicketResponseDTO.java
@@ -0,0 +1,4 @@
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.ticket.DTOs;
+
+public class TicketResponseDTO {
+}
---
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/web/ticket/webMappers/TicketWebMapper.java
@@ -0,0 +1,5 @@
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.ticket.webMappers;
+
+public interface TicketWebMapper
+{
+}
