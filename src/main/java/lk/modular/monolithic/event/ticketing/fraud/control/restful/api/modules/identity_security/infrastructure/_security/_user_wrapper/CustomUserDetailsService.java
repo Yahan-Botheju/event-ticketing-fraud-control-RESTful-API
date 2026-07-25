b11903bdaed1412f2fff73136bd2a897bec790b5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-25 12:09:43 +0530

    change folder location

---
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_user_wrapper/CustomUserDetailsService.java
@@ -0,0 +1,4 @@
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._user_wrapper;
+
+public class CustomUserDetailsService {
+}
--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_user_wrapper/CustomUserDetails.java
+++
@@ -1,4 +0,0 @@
-package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._user_wrapper;
-
-public class CustomUserDetails {
-}
