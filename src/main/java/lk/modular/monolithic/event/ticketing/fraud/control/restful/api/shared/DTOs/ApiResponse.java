1874223d454760d3f1f62926d90a1c497053601f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-25 14:34:08 +0530

    feat: initiate api response fields

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/DTOs/ApiResponse.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/DTOs/ApiResponse.java
@@ -1,4 +1,21 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.DTOs;
 
-public class ApiResponse {
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final T data;
+    private final ErrorDetails errorDetails;
+
+    public ApiResponse(boolean success, T data, ErrorDetails errorDetails) {
+        this.success = success;
+        this.data = data;
+        this.errorDetails = errorDetails;
+    }
+
 }
