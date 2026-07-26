149c7a594bd69a6f1bec708ed2f5f15ac56c4a39
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-26 22:44:03 +0530

    feat: initiate jpa user repo

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/jpa/JpaUserRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/jpa/JpaUserRepository.java
@@ -1,4 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.jpa;
 
-public interface JpaUserRepository {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
 }
