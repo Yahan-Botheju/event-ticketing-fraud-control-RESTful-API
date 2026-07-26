5ce127e7ec93005d72222c2697877cbc40c9f89e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-26 20:30:44 +0530

    feat: initiate redis tempalte as bean through configuration class

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/config/RedisConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/config/RedisConfig.java
@@ -1,4 +1,14 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
 public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory
+    ) {}
 }
