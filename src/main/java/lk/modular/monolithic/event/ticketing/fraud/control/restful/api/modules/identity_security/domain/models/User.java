d16180b3cdd573547c29887acf6b38bfe595f2c4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-26 22:17:15 +0530

    feat: initiate user domain model fields

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/models/User.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/models/User.java
@@ -1,4 +1,21 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+    private Long userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private Role role;
+    private LocalDateTime createdAt;
 }
