759822ddcb64d36b3f5d3b90f1c82e0761979d20
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-27 20:21:07 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/UserRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/UserRepositoryImpl.java
@@ -1,4 +1,21 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user;
 
-public class UserRepositoryImpl {
+
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.jpa.JpaUserRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.persistenceMapper.UserPersistenceMapper;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    //inject required dependencies
+    private final JpaUserRepository jpaUserRepository;
+    private final UserPersistenceMapper userPersistenceMapper;
+
+    public UserRepositoryImpl(
+            JpaUserRepository jpaUserRepository,
+            UserPersistenceMapper userPersistenceMapper
+    ) {
+        this.jpaUserRepository = jpaUserRepository;
+        this.userPersistenceMapper = userPersistenceMapper;
+    }
 }
