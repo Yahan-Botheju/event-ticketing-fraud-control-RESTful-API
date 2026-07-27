72528f820d7b51608b12833950078476bea16b7e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-27 22:17:52 +0530

    feat: initiate save refresh token method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/RedisTokenRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/RedisTokenRepository.java
@@ -1,4 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories;
 
 public interface RedisTokenRepository {
+
+    //save refresh token in redis context
+    void saveRefreshToken(Long userId, String refreshToken, long durationInMs);
 }
