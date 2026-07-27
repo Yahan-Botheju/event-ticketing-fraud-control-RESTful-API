51f246a3274ae8a32ac49f24e9e42f5503a185f3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-27 22:05:05 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_redis/RedisTokenRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_redis/RedisTokenRepositoryImpl.java
@@ -1,4 +1,19 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._redis;
 
-public class RedisTokenRepositoryImpl {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.RedisTokenRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisTokenRepositoryImpl implements RedisTokenRepository {
+
+    //inject required dependencies
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final String refreshTokenPrefix;
+
+    public RedisTokenRepositoryImpl(
+            RedisTemplate<String, Object> redisTemplate,
+            String refreshTokenPrefix
+    ) {
+        this.redisTemplate = redisTemplate;
+        this.refreshTokenPrefix = refreshTokenPrefix;
+    }
 }
