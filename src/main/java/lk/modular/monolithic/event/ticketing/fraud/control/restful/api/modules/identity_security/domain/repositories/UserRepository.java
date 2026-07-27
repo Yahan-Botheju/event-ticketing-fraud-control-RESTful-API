ef6ec6d047b2228ff014034ec7e4ef8f3e857d52
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-27 20:24:16 +0530

    feat: initiate user find ny email method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/UserRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
+
+import java.util.Optional;
+
 public interface UserRepository {
+
+    //find user by email
+    Optional<User> findByEmail(String email);
 }
