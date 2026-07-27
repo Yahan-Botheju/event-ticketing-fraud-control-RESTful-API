b9af2ba6a7371248992c7c343a98460a50cb0f19
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-27 22:28:47 +0530

    feat: create build key method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_redis/RedisTokenRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_redis/RedisTokenRepositoryImpl.java
@@ -3,6 +3,8 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.RedisTokenRepository;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.time.Duration;
+
 public class RedisTokenRepositoryImpl implements RedisTokenRepository {
 
     //inject required dependencies
@@ -15,5 +17,27 @@ public class RedisTokenRepositoryImpl implements RedisTokenRepository {
     ) {
         this.redisTemplate = redisTemplate;
         this.refreshTokenPrefix = refreshTokenPrefix;
+    }
+
+    /*  __HELPER_METHODS__ */
+
+    private String buildKey(Long userId) {
+        return refreshTokenPrefix + userId;
+    }
+
+
+
+    /*  __PUBLIC_METHODS__ */
+
+    //save refresh token in redis context
+    @Override
+    public void saveRefreshToken(
+            Long userId,
+            String refreshToken,
+            long durationInMs
+    ) {
+        String key = buildKey(userId);
+
+        redisTemplate.opsForValue().set(key, refreshToken, Duration.ofMillis(durationInMs));
     }
 }
