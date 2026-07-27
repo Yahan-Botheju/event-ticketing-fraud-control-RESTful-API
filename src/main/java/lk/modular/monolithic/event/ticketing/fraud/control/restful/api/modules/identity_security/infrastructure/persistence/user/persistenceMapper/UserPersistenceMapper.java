4ce3d684efcba9e2bb6d135a60849ed44d002a2a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-27 20:03:48 +0530

    feat: create user persistence mapper interface

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/persistenceMapper/UserPersistenceMapper.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/persistenceMapper/UserPersistenceMapper.java
@@ -1,4 +1,10 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.persistenceMapper;
 
-public class UserPersistenceMapper {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.entities.UserEntity;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public interface UserPersistenceMapper {
+
 }
