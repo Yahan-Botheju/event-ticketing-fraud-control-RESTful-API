da6b3d6e12a1d0454e5a4dd2cd01d634d80024f8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-28 22:28:30 +0530

    feat: implements with spring security user details wrapper

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_user_wrapper/CustomUserDetails.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_user_wrapper/CustomUserDetails.java
@@ -1,4 +1,6 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._user_wrapper;
 
-public class CustomUserDetails {
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
 }
