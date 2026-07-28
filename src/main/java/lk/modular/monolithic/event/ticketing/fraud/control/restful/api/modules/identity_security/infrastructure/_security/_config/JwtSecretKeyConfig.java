dbdbe945c277aa45efccae3f05b90c25bc28ca3b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-28 11:47:09 +0530

    feat: initiate secret key as separate configuration class to inject the key

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/JwtSecretKeyConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/JwtSecretKeyConfig.java
@@ -1,4 +1,20 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._config;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
 public class JwtSecretKeyConfig {
+    @Bean
+    public SecretKey secretKey(
+            @Value("${application.security.jwt.secret-key}") String secretKey
+    ) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
