3c870cc78a19098628fa65e72f40cb55be12b41f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-28 13:48:17 +0530

    feat: initiate get claim from token method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/JwtTokenProvider.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/JwtTokenProvider.java
@@ -1,9 +1,14 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories;
 
+import io.jsonwebtoken.Claims;
+
 public interface JwtTokenProvider {
     //generate access token
     String generateAccessToken(Long userId, String email, String role);
 
     //generate refresh token
     String generateRefreshToken(String email);
+
+    //get claim from token
+    Claims getClaimsFromToken(String token);
 }
