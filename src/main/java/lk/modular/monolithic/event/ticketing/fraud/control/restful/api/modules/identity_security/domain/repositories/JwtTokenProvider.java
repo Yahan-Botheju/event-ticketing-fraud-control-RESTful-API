994e0a2d42b5ba353fffd86f060df8dc8671e295
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-28 12:53:12 +0530

    feat: initiate generate refreh token method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/JwtTokenProvider.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/JwtTokenProvider.java
@@ -1,5 +1,9 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories;
 
 public interface JwtTokenProvider {
+    //generate access token
     String generateAccessToken(Long userId, String email, String role);
+
+    //generate refresh token
+    String generateRefreshToken(String email);
 }
