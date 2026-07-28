84027a05e9cb064f027facf17bab5b79e910354d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-28 22:54:15 +0530

    feat: implements with spring user details service interface

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_user_wrapper/CustomUserDetailsService.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_user_wrapper/CustomUserDetailsService.java
@@ -1,4 +1,10 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._user_wrapper;
 
-public class CustomUserDetailsService {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+
 }
