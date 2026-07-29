7c7fd6b4deab795e9ec4581005e01037f2ad2dd8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-29 21:44:44 +0530

    feat: initiate filter as bean through configuration class

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_jwtBeanConfig/JwtAuthenticationFilterBeanConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_jwtBeanConfig/JwtAuthenticationFilterBeanConfig.java
@@ -1,4 +1,21 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._config._jwtBeanConfig;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.JwtTokenProvider;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.RedisTokenRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._user_wrapper.CustomUserDetailsService;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Configuration
 public class JwtAuthenticationFilterBeanConfig {
+    @Bean
+    public OncePerRequestFilter oncePerRequestFilter(
+            JwtTokenProvider jwtTokenProvider,
+            CustomUserDetailsService customUserDetailsService,
+            RedisTokenRepository redisTokenRepository
+    ) {
+        return new JwtAuthenticationFilter(jwtTokenProvider, customUserDetailsService, redisTokenRepository);
+    }
 }
