c30546c4d08b887bfb6ddfee5f0069829ed7e016
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-29 23:26:04 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/SecurityConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/SecurityConfig.java
@@ -1,4 +1,19 @@
-package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security.config;
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._config;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    //inject required dependencies
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 }
