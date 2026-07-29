40d1b0e84e6e17c124fa6bed459837495f91af9b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-29 23:27:59 +0530

    feat: generate spring security filter chain config method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/SecurityConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._config;
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,4 +19,10 @@ public class SecurityConfig {
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
+
+    //spring security filter chain config method
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {}
 }
