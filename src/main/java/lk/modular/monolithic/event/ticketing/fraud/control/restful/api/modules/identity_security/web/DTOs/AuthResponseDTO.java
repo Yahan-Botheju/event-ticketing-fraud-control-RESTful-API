bd891fbb27a18f4827f960b9c3e2cc83860b0620
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-30 11:40:26 +0530

    feat: initiate auth response dto fields

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/web/DTOs/AuthResponseDTO.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/web/DTOs/AuthResponseDTO.java
@@ -1,4 +1,17 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.web.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponseDTO {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long userId;
+    private String email;
+    private String role;
 }
