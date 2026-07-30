458c7f35315ac3c2157eecbe4e489ec1800e65a0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-30 14:12:52 +0530

    ref: change interface name

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/IdentityProvider.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/IdentityProvider.java
@@ -1,5 +1,5 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories;
 
-public interface PasswordHasher {
+public interface IdentityProvider {
     String encode(String rawPassword);
 }
