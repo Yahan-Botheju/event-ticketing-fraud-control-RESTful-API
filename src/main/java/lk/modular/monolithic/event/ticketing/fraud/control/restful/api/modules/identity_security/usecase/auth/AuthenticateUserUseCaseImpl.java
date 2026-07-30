b78f1915192af582116a9294305b2a42bbc08116
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-30 14:20:01 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/auth/AuthenticateUserUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/auth/AuthenticateUserUseCaseImpl.java
@@ -1,4 +1,20 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.auth;
 
-public class AuthenticateUserUseCaseImpl {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.IdentityProvider;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
+
+public class AuthenticateUserUseCaseImpl implements AuthenticateUserUseCase {
+
+    //inject required dependencies
+    private final IdentityProvider identityProvider;
+    private final UserRepository userRepository;
+
+
+    public AuthenticateUserUseCaseImpl(
+            IdentityProvider identityProvider,
+            UserRepository userRepository
+    ) {
+        this.identityProvider = identityProvider;
+        this.userRepository = userRepository;
+    }
 }
