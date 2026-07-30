e2759bff28258f336a7a52d4411f9ef7812dda99
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-30 14:56:43 +0530

    fix: folder name change

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/register/RegisterUserUseCase.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/register/RegisterUserUseCase.java
@@ -1,4 +1,4 @@
-package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.auth;
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.register;
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
 
