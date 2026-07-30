8d9136d85d35e1fde71056c0ed756a736fe35787
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-30 14:26:30 +0530

    feat: set user role

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/auth/AuthenticateUserUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/auth/AuthenticateUserUseCaseImpl.java
@@ -1,6 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.auth;
 
 import jakarta.transaction.Transactional;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.Role;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.IdentityProvider;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
@@ -29,7 +30,11 @@ public class AuthenticateUserUseCaseImpl implements AuthenticateUserUseCase {
             throw new ConflictException("Email already registered..!!");
         }
 
+        //encode password using  identity domain repo
         String encodedPassword = identityProvider.encode(user.getPassword());
+
+        //if role is empty set as default role
+        Role userRole = user.getRole() != null ? user.getRole() : Role.ATTENDEE;
 
 
     }
