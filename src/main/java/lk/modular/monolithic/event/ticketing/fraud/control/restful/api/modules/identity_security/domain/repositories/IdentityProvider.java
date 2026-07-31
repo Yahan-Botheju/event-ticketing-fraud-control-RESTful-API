8099bff4b7718008ca598a5961902fd2ed25a6b8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-31 15:06:07 +0530

    feat: initiate domain repo for common usage for password cocoder and auth manager and initiate realted methods

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/IdentityProvider.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/IdentityProvider.java
@@ -1,9 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.AuthenticatedUser;
+
 public interface IdentityProvider {
     //shared password encoder
     String encodePassword(String rawPassword);
 
     //shared authentication operations
-    Object authenticate(String username, String password);
+    AuthenticatedUser authenticateUser(String username, String password);
 }
