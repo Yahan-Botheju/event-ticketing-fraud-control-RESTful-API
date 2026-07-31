9f3fc1d2cdf245d97ee0f10d3da90a1ff6d079c0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-31 11:39:23 +0530

    feat: initiate authentication manager method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/IdentityProvider.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/domain/repositories/IdentityProvider.java
@@ -1,5 +1,9 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories;
 
 public interface IdentityProvider {
-    String encode(String rawPassword);
+    //shared password encoder
+    String encodePassword(String rawPassword);
+
+    //shared authentication operations
+    Object authenticate(String username, String password);
 }
