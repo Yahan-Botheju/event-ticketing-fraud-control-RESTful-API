828b49a04b2d5ea006e74978c7d88568f7c4e306
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-31 15:24:39 +0530

    feat: initiate login user method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/login/LoginUserUseCase.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/login/LoginUserUseCase.java
@@ -1,4 +1,12 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.login;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.AuthenticatedUser;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.AuthenticatedUserResult;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
+
 public interface LoginUserUseCase {
+
+    //login user
+    AuthenticatedUserResult login(String username, String password, HttpServletResponse httpServletResponse);
 }
