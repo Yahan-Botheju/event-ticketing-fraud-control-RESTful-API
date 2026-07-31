ee3ffd6da3a9d689fc9a4c1e6ef47e8b01f48c0f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-31 12:24:24 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/login/LoginUserUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/login/LoginUserUseCaseImpl.java
@@ -1,4 +1,23 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.login;
 
-public class LoginUserUseCase {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.IdentityProvider;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.JwtTokenProvider;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.RedisTokenRepository;
+
+public class LoginUserUseCaseImpl implements LoginUserUseCase {
+
+    //inject required dependencies
+    private final IdentityProvider identityProvider;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTokenRepository redisTokenRepository;
+
+    public LoginUserUseCaseImpl(
+            IdentityProvider identityProvider,
+            JwtTokenProvider jwtTokenProvider,
+            RedisTokenRepository redisTokenRepository
+    ) {
+        this.identityProvider = identityProvider;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.redisTokenRepository = redisTokenRepository;
+    }
 }
