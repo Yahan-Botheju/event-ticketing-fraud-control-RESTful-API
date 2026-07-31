11ed2326d4450eb4d36c85f3127ece43f160d222
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-07-31 11:40:30 +0530

    feat: create authentication manager method and encoder method as return from one method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/ApplicationConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_config/ApplicationConfig.java
@@ -1,12 +1,15 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._config;
 
-import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.PasswordHasher;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.IdentityProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,11 +30,6 @@ public class ApplicationConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public PasswordHasher passwordHasher(PasswordEncoder passwordEncoder) {
-        return passwordEncoder::encode;
-    }
-
     //initiate auth manager
     @Bean
     public AuthenticationManager authenticationManager(
@@ -47,5 +45,31 @@ public class ApplicationConfig {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
         return daoAuthenticationProvider;
+    }
+
+    //shared domain repository bean operations
+    @Bean
+    public IdentityProvider identityProvider(
+            PasswordEncoder passwordEncoder,
+            AuthenticationManager authenticationManager
+    ) {
+        return new IdentityProvider() {
+            //password encoder
+            @Override
+            public String encodePassword(String rawPassword) {
+                return passwordEncoder.encode(rawPassword);
+            }
+            //authentication manager
+            @Override
+            public Object authenticate(
+                    String username, String password
+            ) throws AuthenticationException {
+                Authentication authentication = authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(username, password)
+                );
+                return authentication.getPrincipal();
+            }
+
+        };
     }
 }
