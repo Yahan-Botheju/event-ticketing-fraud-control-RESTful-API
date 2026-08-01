86241e2a8cb5060db335a8ef5329995401320a0e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-01 13:38:29 +0530

    feat: initate refresk token usecase impl as service class through config class

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfig.java
@@ -6,6 +6,8 @@ import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.i
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.login.LoginUserUseCase;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.login.LoginUserUseCaseImpl;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.refreshToken.RefreshTokenUseCase;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.refreshToken.RefreshTokenUseCaseImpl;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.register.RegisterUserUseCase;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.register.RegisterUserUseCaseImpl;
 import org.springframework.context.annotation.Bean;
@@ -31,5 +33,15 @@ public class UseCaseBeanConfig {
             RedisTokenRepository redisTokenRepository
     ) {
         return new LoginUserUseCaseImpl(identityProvider, jwtTokenProvider,redisTokenRepository);
+    }
+
+    //refresh token usecase bean config
+    @Bean
+    public RefreshTokenUseCase refreshTokenUseCase(
+            UserRepository userRepository,
+            JwtTokenProvider jwtTokenProvider,
+            RedisTokenRepository redisTokenRepository
+    ){
+        return new RefreshTokenUseCaseImpl(userRepository,jwtTokenProvider,redisTokenRepository);
     }
 }
