9097f99c7db28f296c6f073cef02896b9ea4e636
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-01 22:52:28 +0530

    fix: import proper jackson library

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/config/RedisConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/config/RedisConfig.java
@@ -1,20 +1,18 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.config;
 
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.*;
-
-
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    //refresh token refix
     @Value("${application.security.redis.refresh-token-prefix}")
     private String refreshTokenPrefix;
 
@@ -23,31 +21,29 @@ public class RedisConfig {
         return refreshTokenPrefix;
     }
 
-
-    // custom serialize bean template
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
             RedisConnectionFactory redisConnectionFactory,
             @Qualifier("redisObjectMapper") ObjectMapper redisObjectMapper
     ) {
-        //create template object
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 
-        //set redis template to connection factory
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
 
-        //serialize key as string
+        // Value Serializer
+        GenericJackson2JsonRedisSerializer jsonSerializer =
+                new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+
+        // Key Serializer
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 
-        //initiate redis serializer
-        GenericJacksonJsonRedisSerializer jsonSerializer = new GenericJacksonJsonRedisSerializer(redisObjectMapper);
-
-        //serialize values
+        // Value Serializer
         redisTemplate.setValueSerializer(jsonSerializer);
         redisTemplate.setHashValueSerializer(jsonSerializer);
 
         redisTemplate.afterPropertiesSet();
+
         return redisTemplate;
     }
 }
