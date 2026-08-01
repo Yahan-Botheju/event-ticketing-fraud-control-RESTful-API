64e4d1a1d54fbaea1a040bd6fc267c85c0e285db
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-01 14:14:12 +0530

    feat: generate method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/refreshToken/RefreshTokenUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/refreshToken/RefreshTokenUseCaseImpl.java
@@ -1,5 +1,6 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.refreshToken;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.records.AuthenticatedUserResult;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.JwtTokenProvider;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.RedisTokenRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
@@ -20,5 +21,11 @@ public class RefreshTokenUseCaseImpl implements RefreshTokenUseCase {
         this.userRepository = userRepository;
         this.jwtTokenProvider = jwtTokenProvider;
         this.redisTokenRepository = redisTokenRepository;
+    }
+
+    //active new access token when its expired
+    @Override
+    public AuthenticatedUserResult execute(String refreshToken) {
+        return null;
     }
 }
