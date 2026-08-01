b507d8988471ee60b519a0138637d43cbb2e9588
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-01 16:03:50 +0530

    feat: injecte required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/logout/LogoutUserUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/logout/LogoutUserUseCaseImpl.java
@@ -1,4 +1,13 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.logout;
 
-public class LogoutUserUseCaseImpl {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.RedisTokenRepository;
+
+public class LogoutUserUseCaseImpl implements  LogoutUserUseCase {
+
+    //inject required dependencies
+    private final RedisTokenRepository redisTokenRepository;
+
+    public LogoutUserUseCaseImpl(RedisTokenRepository redisTokenRepository) {
+        this.redisTokenRepository = redisTokenRepository;
+    }
 }
