aeafac6ed12e9ca160aa3c5cd708c022dabab3ce
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-01 16:03:31 +0530

    feat: initiate logout user usecase as service class through config class

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfig.java
@@ -6,6 +6,8 @@ import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.i
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.login.LoginUserUseCase;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.login.LoginUserUseCaseImpl;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.logout.LogoutUserUseCase;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.logout.LogoutUserUseCaseImpl;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.refreshToken.RefreshTokenUseCase;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.refreshToken.RefreshTokenUseCaseImpl;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase.register.RegisterUserUseCase;
@@ -43,5 +45,13 @@ public class UseCaseBeanConfig {
             RedisTokenRepository redisTokenRepository
     ){
         return new RefreshTokenUseCaseImpl(userRepository,jwtTokenProvider,redisTokenRepository);
+    }
+
+    //logout usecase bean config
+    @Bean
+    public LogoutUserUseCase logoutUserUseCase(
+            RedisTokenRepository redisTokenRepository
+    ){
+        return new LogoutUserUseCaseImpl(redisTokenRepository);
     }
 }
