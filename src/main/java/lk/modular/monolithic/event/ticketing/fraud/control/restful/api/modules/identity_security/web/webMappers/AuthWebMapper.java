33a93094ff2fa7226a93391a62f04118a9d5816d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-01 22:57:22 +0530

    feat: setup register endpoint

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/web/webMappers/AuthWebMapper.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/web/webMappers/AuthWebMapper.java
@@ -1,4 +1,29 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.web.webMappers;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.web.DTOs.AuthResponseDTO;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.web.DTOs.LoginRequestDTO;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.web.DTOs.RegisterRequestDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface AuthWebMapper {
+
+    /* requestDTO to domain model */
+
+    //__REGISTER__
+    User registerDomainModel(RegisterRequestDTO registerRequestDTO);
+
+    //__LOGIN__
+    User loginDomainModel(LoginRequestDTO loginRequestDTO);
+
+    /* domain model to responseDTO */
+
+    AuthResponseDTO authResponseDTO(User user);
+
+
+
+
+
+
 }
