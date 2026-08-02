ab8f1f0aed057abfa6a191b41833b00dfa138259
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-02 20:53:21 +0530

    feat: create annotation class

---
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/_resolver/CurrentUserIdArgumentResolver.java
@@ -0,0 +1,4 @@
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._resolver;
+
+public class CurrentUserIdArgumentResolver {
+}
---
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/web/annotation/CurrentUserId.java
@@ -0,0 +1,11 @@
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.web.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUserId {
+}
