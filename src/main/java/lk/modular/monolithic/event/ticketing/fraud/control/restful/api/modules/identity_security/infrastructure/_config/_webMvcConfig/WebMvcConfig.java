802bb9a0a3bea5e1b044cb60eb2e52296d7a2f69
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-02 21:08:53 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_webMvcConfig/WebMvcConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_config/_webMvcConfig/WebMvcConfig.java
@@ -1,4 +1,21 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._config._webMvcConfig;
 
-public class WebMvcConfig {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._resolver.CurrentUserIdArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    //inject required dependencies
+    private final CurrentUserIdArgumentResolver currentUserIdArgumentResolver;
+
+    public WebMvcConfig(CurrentUserIdArgumentResolver currentUserIdArgumentResolver) {
+        this.currentUserIdArgumentResolver = currentUserIdArgumentResolver;
+    }
+
+
 }
