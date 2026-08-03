cee3290755d88778965a1489f0dbd20af46781e1
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-03 23:27:48 +0530

    feat: initiate ticket persistence as sedrvice through config class

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/_config/_persistenceBeanConfig/PersistenceBeanConfig.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/_config/_persistenceBeanConfig/PersistenceBeanConfig.java
@@ -1,19 +1,34 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure._config._persistenceBeanConfig;
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.EventRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.event.EventRepositoryImpl;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.event.jpa.JpaEventRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.event.persistenceMapper.EventPersistenceMapper;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.TicketRepositoryImpl;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.jpa.JpaTicketRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.persistenceMapper.TicketPersistenceMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceBeanConfig {
+
+    //event persistence implementation bean config
     @Bean
     public EventRepository eventRepository(
             JpaEventRepository jpaEventRepository,
             EventPersistenceMapper eventPersistenceMapper
     ) {
         return new EventRepositoryImpl(jpaEventRepository, eventPersistenceMapper);
+    }
+
+    //ticket persistence implementation bean config
+    @Bean
+    public TicketRepository ticketRepository(
+            JpaTicketRepository jpaTicketRepository,
+            TicketPersistenceMapper ticketPersistenceMapper
+    ){
+        return new TicketRepositoryImpl(jpaTicketRepository, ticketPersistenceMapper);
     }
 }
