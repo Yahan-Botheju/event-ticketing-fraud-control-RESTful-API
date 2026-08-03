870fdfd522af95ca0cefdc0ed5a697399388ee92
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-03 23:31:02 +0530

    feat: initiate ticket persistence mapper

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket/persistenceMapper/TicketPersistenceMapper.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket/persistenceMapper/TicketPersistenceMapper.java
@@ -1,4 +1,15 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.persistenceMapper;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.entities.TicketEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface TicketPersistenceMapper {
+
+    //domain model to entity
+    TicketEntity toEntity(Ticket ticket);
+
+    //entity to domain model
+    Ticket toTicket(TicketEntity ticketEntity);
 }
