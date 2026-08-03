b38df90cc9feeb4426e280b656ebadbea9e00256
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-03 22:32:36 +0530

    feat: initiate event jpa repo

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/event/jpa/JpaEventRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/event/jpa/JpaEventRepository.java
@@ -1,4 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.event.jpa;
 
-public interface JpaEventRepository {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.event.entities.EventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaEventRepository extends JpaRepository<EventEntity, Long> {
 }
