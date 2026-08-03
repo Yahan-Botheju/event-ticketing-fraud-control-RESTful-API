4532f4a64bb8e7a1d09f256b6a1a19c86cd1833e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-03 19:36:23 +0530

    feat: initiate ticket domain model fields

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/models/Ticket.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/models/Ticket.java
@@ -1,4 +1,29 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Ticket {
+    private Long ticketId;
+    private String ticketCode;
+
+    private Long eventId;
+
+    private Long ownerId;
+
+    private BigDecimal ticketPrice;
+    private TicketStatus ticketStatus;
+
+    private LocalDateTime purchasedAt;
+    private LocalDateTime scannedAt;
+
 }
