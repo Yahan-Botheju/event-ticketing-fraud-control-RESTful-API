1b8193c41f2fa98194f0f00e9c09e2f19b12a262
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-04 11:01:29 +0530

    feat: create ticket find by functionality

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket/TicketRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket/TicketRepositoryImpl.java
@@ -1,8 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.jpa.JpaTicketRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.persistenceMapper.TicketPersistenceMapper;
+
+import java.util.Optional;
 
 public class TicketRepositoryImpl implements TicketRepository {
 
@@ -16,5 +19,13 @@ public class TicketRepositoryImpl implements TicketRepository {
     ) {
         this.jpaTicketRepository = jpaTicketRepository;
         this.ticketPersistenceMapper = ticketPersistenceMapper;
+    }
+
+    /* __HELPER_METHOD__ */
+
+    //ticket find by id
+    @Override
+    public Optional<Ticket> findById(Long ticketId) {
+        return jpaTicketRepository.findById(ticketId).map(ticketPersistenceMapper::toDomainModel);
     }
 }
