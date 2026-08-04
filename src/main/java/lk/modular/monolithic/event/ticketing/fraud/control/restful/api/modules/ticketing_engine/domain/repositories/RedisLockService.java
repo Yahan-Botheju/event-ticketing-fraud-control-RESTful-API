f25b7849d4f9a06d6cde24005a39cad37c200556
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-04 22:44:46 +0530

    feat: initiate distributed lock method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/RedisLockService.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/RedisLockService.java
@@ -1,4 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories;
 
 public interface RedisLockService {
+
+    //get distributed lock
+    boolean acquireLock(String lockKey, String lockValue, long expireTimeInSeconds);
 }
