0ef7187af799f1dac0754c5e5b04ee7e34adf2b9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-04 09:56:43 +0530

    feat: initiate save event method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/EventRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/EventRepository.java
@@ -1,4 +1,9 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Event;
+
 public interface EventRepository {
+
+    //save event
+    Event save(Event event);
 }
