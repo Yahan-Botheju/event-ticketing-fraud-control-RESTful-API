2fae67a652df731b13a19feb5f4f3d73ba23cd5c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-04 10:05:04 +0530

    feat: initiate event find by id method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/EventRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/EventRepository.java
@@ -2,8 +2,14 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Event;
 
+import java.util.Optional;
+
 public interface EventRepository {
+
+    //event find by id
+    Optional<Event> findById(Long eventId);
 
     //save event
     Event save(Event event);
+
 }
