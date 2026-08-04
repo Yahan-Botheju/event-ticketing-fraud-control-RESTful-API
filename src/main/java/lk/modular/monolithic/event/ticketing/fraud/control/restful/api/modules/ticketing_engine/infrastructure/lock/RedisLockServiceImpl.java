0c13dbbe66d2314768ba0e4b543996284bdb62bd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-04 22:39:28 +0530

    fix: inject redis lock script

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/lock/RedisLockServiceImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/lock/RedisLockServiceImpl.java
@@ -2,13 +2,19 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.RedisLockService;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
 
 public class RedisLockServiceImpl implements RedisLockService {
 
     //inject required dependencies
     private final StringRedisTemplate stringRedisTemplate;
+    private final RedisScript<Long> releaseRedisScript;
 
-    public RedisLockServiceImpl(StringRedisTemplate stringRedisTemplate) {
+    public RedisLockServiceImpl(
+            StringRedisTemplate stringRedisTemplate,
+            RedisScript<Long>  releaseRedisScript
+    ) {
         this.stringRedisTemplate = stringRedisTemplate;
+        this.releaseRedisScript = releaseRedisScript;
     }
 }
