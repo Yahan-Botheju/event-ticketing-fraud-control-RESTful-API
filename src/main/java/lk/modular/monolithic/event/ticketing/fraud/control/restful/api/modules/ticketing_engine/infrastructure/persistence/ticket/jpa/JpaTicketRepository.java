bfe1cc87751ace3a1b33cd6aa8721ddbe7ee5108
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-04 11:56:22 +0530

    feat: initiate custom query

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket/jpa/JpaTicketRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket/jpa/JpaTicketRepository.java
@@ -3,5 +3,10 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket.entities.TicketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaTicketRepository extends JpaRepository<TicketEntity, Long> {
+
+    //ticket find by code
+    Optional<TicketEntity> findByTicketCode(String ticketCode);
 }
