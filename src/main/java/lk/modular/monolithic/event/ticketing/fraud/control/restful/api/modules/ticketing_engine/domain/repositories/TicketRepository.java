346ba73ba63651e3e6ff6471519a95b2d1a7af1b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-04 11:00:57 +0530

    fat: initiate ticket find by id method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/TicketRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/TicketRepository.java
@@ -1,4 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
+
+import java.util.Optional;
+
 public interface TicketRepository {
+
+    //find ticket by id
+    Optional<Ticket> findById(Long ticketId);
 }
