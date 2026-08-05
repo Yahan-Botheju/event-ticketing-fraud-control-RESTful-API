a8dd450a49e36c733725150cc04c93073c0c4b5f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-05 20:48:56 +0530

    feat: inject new dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/BuyTicketUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/BuyTicketUseCaseImpl.java
@@ -1,23 +1,36 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.EventRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.RedisLockService;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public class BuyTicketUseCaseImpl implements BuyTicketUseCase {
 
     //inject required dependencies
-    private final TicketRepository ticketRepository;
     private final EventRepository eventRepository;
+    private final TicketRepository ticketRepository;
     private final RedisLockService redisLockService;
+    private final String eventLockPrefix;
 
     public BuyTicketUseCaseImpl(
-            TicketRepository ticketRepository,
             EventRepository eventRepository,
-            RedisLockService redisLockService
+            TicketRepository ticketRepository,
+            RedisLockService redisLockService,
+            String eventLockPrefix
     ) {
-        this.ticketRepository = ticketRepository;
         this.eventRepository = eventRepository;
+        this.ticketRepository = ticketRepository;
         this.redisLockService = redisLockService;
+        this.eventLockPrefix = eventLockPrefix;
+    }
+
+    //buy ticket
+    @Override
+    @Transactional
+    public Ticket execute(Long ticketId, Long userId){
+
+
     }
 }
