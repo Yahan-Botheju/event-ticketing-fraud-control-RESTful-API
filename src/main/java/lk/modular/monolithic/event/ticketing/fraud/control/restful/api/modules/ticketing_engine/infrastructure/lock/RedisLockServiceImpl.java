f92d9b236d4d71abe36067982555ce0ae6258ad2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-05 19:24:53 +0530

    feat: setup initiate lock functionality

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/lock/RedisLockServiceImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/lock/RedisLockServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
 
 import java.time.Duration;
+import java.util.Collections;
 
 public class RedisLockServiceImpl implements RedisLockService {
 
@@ -31,5 +32,16 @@ public class RedisLockServiceImpl implements RedisLockService {
                 .setIfAbsent(lockKey, lockValue, Duration.ofSeconds(expireTimeInSeconds));
 
         return Boolean.TRUE.equals(success);
+    }
+
+    //release lock
+    @Override
+    public boolean releaseLock(String lockKey, String lockValue) {
+        Long result = stringRedisTemplate.execute(
+                this.releaseRedisScript,
+                Collections.singletonList(lockKey),
+                lockValue
+        );
+        return Long.valueOf(1L).equals(result);
     }
 }
