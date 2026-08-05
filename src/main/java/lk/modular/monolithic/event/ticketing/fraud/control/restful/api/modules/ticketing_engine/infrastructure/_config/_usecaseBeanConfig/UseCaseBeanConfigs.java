6eb9638b1b503d67b6a2502de3282fe6ca64d03b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-05 19:45:26 +0530

    feat: enable transactional

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfigs.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfigs.java
@@ -5,8 +5,10 @@ import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.t
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event.CreateEventUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class UseCaseBeanConfigs {
 
     //create event usecase
