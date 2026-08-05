afceda16f360eea5e8d6b257557bb2cfe732ba4f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-05 19:52:54 +0530

    feat: initiate create ticket method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/CreateEventUseCase.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/CreateEventUseCase.java
@@ -1,4 +1,9 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Event;
+
 public interface CreateEventUseCase {
+
+    //create event
+    Event execute(Event event);
 }
