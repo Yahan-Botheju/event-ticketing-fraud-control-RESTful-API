ce9346929e14204748341a331139458bd60c8287
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-05 19:53:33 +0530

    feat: check ticket availability

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/CreateEventUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/CreateEventUseCaseImpl.java
@@ -1,6 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Event;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.EventRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.exception.InvalidTicketException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 public class CreateEventUseCaseImpl implements CreateEventUseCase {
 
@@ -11,5 +16,17 @@ public class CreateEventUseCaseImpl implements CreateEventUseCase {
             EventRepository eventRepository
     ) {
         this.eventRepository = eventRepository;
+    }
+
+    /* __PUBLIC_METHODS__ */
+
+    //create event
+    @Override
+    @Transactional
+    public Event execute(Event event){
+        //check ticket availability
+        if(event.getEventTotalTickets() == null || event.getEventTicketPrice().compareTo(BigDecimal.ZERO) <= 0){
+            throw new InvalidTicketException("Total tickets must be greater than zero");
+        }
     }
 }
