7394785fad3aaca0bacb4947892eb891fd11bb56
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-06 22:03:50 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/TransferTicketUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/TransferTicketUseCaseImpl.java
@@ -1,4 +1,13 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.Ticket;
 
-public class TransferTicketUseCaseImpl {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
+
+public class TransferTicketUseCaseImpl implements  TransferTicketUseCase {
+
+    //inject required dependencies
+    private final TicketRepository ticketRepository;
+
+    public TransferTicketUseCaseImpl(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
 }
