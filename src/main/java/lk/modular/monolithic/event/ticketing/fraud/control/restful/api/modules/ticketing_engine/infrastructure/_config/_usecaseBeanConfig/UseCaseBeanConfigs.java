c159dceba95e4aff1e502e9c344e8286777b856f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-06 20:39:04 +0530

    feat: initiate in usecase impl

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfigs.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/_config/_usecaseBeanConfig/UseCaseBeanConfigs.java
@@ -126,7 +126,8 @@ public class UseCaseBeanConfigs {
         return new ScanTicketUseCaseImpl(
                 ticketRepository,
                 redisLockService,
-                ticketScanLockPrefix
+                ticketScanLockPrefix,
+                ticketScanExpirationSeconds
         );
     }
 }
