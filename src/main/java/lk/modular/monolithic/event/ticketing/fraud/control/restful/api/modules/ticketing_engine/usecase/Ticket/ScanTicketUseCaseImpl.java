4801c6338c37fe3a7270433761f3317f467973ea
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-06 21:42:36 +0530

    feat: generate scan ticket key

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/ScanTicketUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/ScanTicketUseCaseImpl.java
@@ -1,5 +1,6 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.Ticket;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.RedisLockService;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
 
@@ -21,6 +22,13 @@ public class ScanTicketUseCaseImpl implements  ScanTicketUseCase {
         this.redisLockService = redisLockService;
         this.ticketScanLockPrefix = ticketScanLockPrefix;
         this.ticketScanExpirationSeconds = ticketScanExpirationSeconds;
+    }
+
+    //scan ticket
+    @Override
+    public Ticket execute(String ticketCode){
+        //generate scan ticket key
+        String localKey = ticketScanLockPrefix + ticketCode;
     }
 
 }
