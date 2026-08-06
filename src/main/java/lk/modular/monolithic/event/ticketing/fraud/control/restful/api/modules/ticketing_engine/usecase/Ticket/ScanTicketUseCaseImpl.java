5a12e4b00ea7d70044c5d55fd06267727bd67067
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-06 20:16:41 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/ScanTicketUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/ScanTicketUseCaseImpl.java
@@ -1,4 +1,19 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.Ticket;
 
-public class ScanTicketUseCaseImpl {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.RedisLockService;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
+
+public class ScanTicketUseCaseImpl implements  ScanTicketUseCase {
+
+    //inject required dependencies
+    private final TicketRepository ticketRepository;
+    private final RedisLockService redisLockService;
+
+    public ScanTicketUseCaseImpl(
+            TicketRepository ticketRepository,
+            RedisLockService redisLockService
+    ) {
+        this.ticketRepository = ticketRepository;
+        this.redisLockService = redisLockService;
+    }
 }
