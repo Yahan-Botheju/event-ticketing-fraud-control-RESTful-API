0672dfadf319c93c3af572d336197311985dca81
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-07 19:50:28 +0530

    feat: initiate ticket transfer method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/TransferTicketUseCase.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/Ticket/TransferTicketUseCase.java
@@ -1,4 +1,8 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.Ticket;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
+
 public interface TransferTicketUseCase {
+    //transfer a ticket
+    Ticket transferTicket(Long ticketId, Long currentOwnerId, Long newOwnerId);
 }
