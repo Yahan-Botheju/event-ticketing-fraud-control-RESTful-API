35cdedd0ed556729eb0be762153045c90e26eff3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-07 21:15:25 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/web/event/controllers/EventController.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/web/event/controllers/EventController.java
@@ -1,4 +1,23 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.event.controllers;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event.CreateEventUseCase;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.event.webMappers.EventWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/events")
 public class EventController {
+
+    //inject required dependencies
+    private final CreateEventUseCase createEventUseCase;
+    private final EventWebMapper eventWebMapper;
+
+    public EventController(
+            CreateEventUseCase createEventUseCase,
+            EventWebMapper eventWebMapper
+    ) {
+        this.createEventUseCase = createEventUseCase;
+        this.eventWebMapper = eventWebMapper;
+    }
 }
