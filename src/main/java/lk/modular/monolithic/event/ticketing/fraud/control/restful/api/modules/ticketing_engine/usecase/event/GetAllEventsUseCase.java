e79a0a5f30bd571513ffa496a47126eef10a7512
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-08 20:15:04 +0530

    feat: initiate get all event method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllEventsUseCase.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllEventsUseCase.java
@@ -1,4 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Event;
+
+import java.util.List;
+
 public interface GetAllEventsUseCase {
+
+    //get all events
+    List<Event> getAllEvents();
 }
