96fb8f9ced6684054458cece3bb9ff89e28cb5cc
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-08 20:14:03 +0530

    feat: injecte required dependenices

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllEventsUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllEventsUseCaseImpl.java
@@ -1,4 +1,13 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event;
 
-public class GetAllEventsUseCaseImpl {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.EventRepository;
+
+public class GetAllEventsUseCaseImpl implements GetAllEventsUseCase {
+
+    //inject required dependencies
+    private final EventRepository eventRepository;
+
+    public GetAllEventsUseCaseImpl(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
 }
