931181c54ca22b0c74f8b3ea32f114dced88a201
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-10 15:20:43 +0530

    feat: initiate get all ticket clear

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllTicketsUseCase.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllTicketsUseCase.java
@@ -1,4 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
+
+import java.util.List;
+
 public interface GetAllTicketsUseCase {
+
+    //find all tickets of a user
+    List<Ticket> findAllTicketsByUserId(Long userId);
 }
