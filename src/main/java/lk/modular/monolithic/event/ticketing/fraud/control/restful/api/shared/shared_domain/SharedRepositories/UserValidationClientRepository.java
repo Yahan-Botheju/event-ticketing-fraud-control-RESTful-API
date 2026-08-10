28353459bfb8ef2a2a1b13edc340e658606f8d03
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-10 15:25:53 +0530

    feat: create shared port for validate user for usage to ticket engine module

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/shared_domain/SharedRepositories/UserValidationClientRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/shared_domain/SharedRepositories/UserValidationClientRepository.java
@@ -1,4 +1,11 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.shared_domain.SharedRepositories;
 
-public interface UserValidationRepository {
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
+
+import java.util.Optional;
+
+public interface UserValidationClientRepository {
+
+    //user find by id
+    Optional<User> userValidateById(Long userId);
 }
