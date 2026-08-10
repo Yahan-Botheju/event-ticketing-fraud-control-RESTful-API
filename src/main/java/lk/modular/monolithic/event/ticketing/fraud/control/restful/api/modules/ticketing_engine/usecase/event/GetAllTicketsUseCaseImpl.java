9006a9c4a813d9d445375c12cf0880afcf182f4b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-10 15:35:03 +0530

    feat: create functionality of find my tickets

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllTicketsUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllTicketsUseCaseImpl.java
@@ -2,6 +2,7 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.exception.ResourceNotFoundException;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.shared_domain.SharedRepositories.UserValidationClientRepository;
 
 import java.util.List;
@@ -22,9 +23,10 @@ public class GetAllTicketsUseCaseImpl implements  GetAllTicketsUseCase {
 
     //find all tickets of a user
     @Override
-    public List<Ticket> findAllTicketsByUserId(Long userId) {
+    public List<Ticket> findMyTickets(Long userId) {
+        userValidationClientRepository.userValidateById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found" + userId));
 
-
-        return List.of();
+        return ticketRepository.findMyTickets(userId);
     }
 }
