d23c51bf46a75ef369466948199801f6cdbb8241
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-10 13:14:38 +0530

    feat: setup transfer ticket endpoint

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/web/ticket/controllers/TicketController.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/web/ticket/controllers/TicketController.java
@@ -1,19 +1,18 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.ticket.controllers;
 
+import jakarta.validation.Valid;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.Ticket.BuyTicketUseCase;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.Ticket.ScanTicketUseCase;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.Ticket.TransferTicketUseCase;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.ticket.DTOs.TicketResponseDTO;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.ticket.DTOs.TransferTicketRequestDTO;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.web.ticket.webMappers.TicketWebMapper;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.DTOs.ApiResponse;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.web_resolver.annotation.CurrentUserId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/tickets")
@@ -57,6 +56,22 @@ public class TicketController {
     ){
         Ticket scanTicket = scanTicketUseCase.execute(ticketCode);
         TicketResponseDTO responseDTO = ticketWebMapper.toResponseDTO(scanTicket);
+
+        return ResponseEntity.ok(ApiResponse.success(responseDTO));
+    }
+
+    //transfer ticket
+    @PostMapping("/transfer-ticket")
+    public ResponseEntity<ApiResponse<TicketResponseDTO>> transferTicket(
+            @Valid @RequestBody TransferTicketRequestDTO transferTicketRequestDTO,
+            @CurrentUserId Long userId
+    ){
+        //get newUserId and ticketId from request
+        Long newUserId = transferTicketRequestDTO.getNewOwnerId();
+        Long ticketId = transferTicketRequestDTO.getTicketId();
+
+        Ticket transferTicket = transferTicketUseCase.transferTicket(ticketId, userId, newUserId);
+        TicketResponseDTO responseDTO = ticketWebMapper.toResponseDTO(transferTicket);
 
         return ResponseEntity.ok(ApiResponse.success(responseDTO));
     }
