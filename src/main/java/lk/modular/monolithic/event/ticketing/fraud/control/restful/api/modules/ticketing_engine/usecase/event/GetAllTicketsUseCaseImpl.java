32add79b02b3afe62c10d37d4dc455076cf14c63
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-10 14:56:55 +0530

    feat: inject required dependencies

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllTicketsUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/GetAllTicketsUseCaseImpl.java
@@ -1,13 +1,30 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase.event;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Ticket;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.shared_domain.SharedRepositories.UserValidationClientRepository;
+
+import java.util.List;
 
 public class GetAllTicketsUseCaseImpl implements  GetAllTicketsUseCase {
 
     //inject required dependencies
     private final TicketRepository ticketRepository;
+    private final UserValidationClientRepository userValidationClientRepository;
 
-    public GetAllTicketsUseCaseImpl(TicketRepository ticketRepository) {
+    public GetAllTicketsUseCaseImpl(
+            TicketRepository ticketRepository,
+            UserValidationClientRepository userValidationClientRepository
+    ) {
         this.ticketRepository = ticketRepository;
+        this.userValidationClientRepository = userValidationClientRepository;
+    }
+
+    //find all tickets of a user
+    @Override
+    public List<Ticket> findAllTicketsByUserId(Long userId) {
+
+
+        return List.of();
     }
 }
