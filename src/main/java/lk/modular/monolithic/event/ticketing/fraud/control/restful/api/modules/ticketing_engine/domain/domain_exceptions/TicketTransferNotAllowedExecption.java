6b3ca52483e916f19765e3c40bbc8be13b4492f0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-11 20:57:21 +0530

    fix: change exception name

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/domain_exceptions/TicketTransferNotAllowedExecption.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/domain_exceptions/TicketTransferNotAllowedExecption.java
@@ -1,7 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions;
 
-public class TicketSoldOutException extends RuntimeException {
-    public TicketSoldOutException(String message) {
+public class TicketTransferNotAllowedExecption extends RuntimeException {
+    public TicketTransferNotAllowedExecption(String message) {
         super(message);
     }
 }
