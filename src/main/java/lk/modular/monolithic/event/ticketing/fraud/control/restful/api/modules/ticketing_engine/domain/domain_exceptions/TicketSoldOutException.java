269af89c0272b9a3f6a65ff23c767316b85f3a90
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-11 20:14:46 +0530

    ref: initiate exception class

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/domain_exceptions/TicketSoldOutException.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/domain_exceptions/TicketSoldOutException.java
@@ -1,4 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions;
 
-public class TicketSoldOutException {
+public class TicketSoldOutException extends RuntimeException {
+    public TicketSoldOutException(String message) {
+        super(message);
+    }
 }
