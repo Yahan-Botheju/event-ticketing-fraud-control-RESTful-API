1edfe00e4c0a356032fc088c6d7926a8f0fad295
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-12 12:25:43 +0530

    feat: get authenticated user from spring context and gets user id

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/resolver_user_provider/SpringSecurityUserProviderImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/_security/resolver_user_provider/SpringSecurityUserProviderImpl.java
@@ -1,4 +1,22 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security.resolver_user_provider;
 
-public class SpringSecurityUserProviderImpl {
+
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._user_wrapper.CustomUserDetails;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.exception.UnauthorizedException;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.web_resolver.resolver.SpringSecurityUserProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SpringSecurityUserProviderImpl implements SpringSecurityUserProvider {
+
+    @Override
+    public Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
+            return userDetails.getUser().getUserId();
+        }
+
+        throw new UnauthorizedException("User not authenticated");
+    }
 }
