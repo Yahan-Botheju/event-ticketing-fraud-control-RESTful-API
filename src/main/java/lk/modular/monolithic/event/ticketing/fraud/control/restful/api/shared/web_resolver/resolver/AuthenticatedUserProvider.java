0bedb9b39a679cce8bb18b23f6ab926bfad94afe
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-12 11:57:24 +0530

    fix: change prot name

---
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/web_resolver/resolver/AuthenticatedUserProvider.java
@@ -0,0 +1,5 @@
+package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.web_resolver.resolver;
+
+public interface AuthenticatedUserProvider {
+    String getCurrentUserId();
+}
