9fc1ed20605d8ba38f67e2b8c66f2e94d261b4da
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-12 14:04:43 +0530

    ref: initiate boolan return type method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/shared_domain/SharedRepositories/UserValidationClientRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/shared_domain/SharedRepositories/UserValidationClientRepository.java
@@ -1,11 +1,8 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.shared_domain.SharedRepositories;
 
-import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
-
-import java.util.Optional;
 
 public interface UserValidationClientRepository {
 
     //user find by id
-    Optional<User> userValidateById(Long userId);
+    boolean userValidateById(Long userId);
 }
