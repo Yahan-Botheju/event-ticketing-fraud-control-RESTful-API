4ed81e57f27593a1dd5c4f65240a8ebfc27c1f56
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-12 12:33:59 +0530

    remove unused dependencies imports

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/web_resolver/resolver/CurrentUserIdArgumentResolver.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/web_resolver/resolver/CurrentUserIdArgumentResolver.java
@@ -1,12 +1,8 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.web_resolver.resolver;
 
 import jakarta.annotation.Nonnull;
-import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure._security._user_wrapper.CustomUserDetails;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.web_resolver.annotation.CurrentUserId;
-import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.exception.UnauthorizedException;
 import org.springframework.core.MethodParameter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
