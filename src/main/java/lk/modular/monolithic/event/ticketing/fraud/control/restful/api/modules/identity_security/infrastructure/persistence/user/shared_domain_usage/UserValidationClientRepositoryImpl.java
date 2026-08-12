59eb801fc6a9e7d7698892e99f236e30b7d23614
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-12 13:59:37 +0530

    ref: instead of returning user object just return boolan value to check user

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/shared_domain_usage/UserValidationClientRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/infrastructure/persistence/user/shared_domain_usage/UserValidationClientRepositoryImpl.java
@@ -1,11 +1,8 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.shared_domain_usage;
 
-import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.jpa.JpaUserRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.infrastructure.persistence.user.persistenceMapper.UserPersistenceMapper;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.shared_domain.SharedRepositories.UserValidationClientRepository;
-
-import java.util.Optional;
 
 public class UserValidationClientRepositoryImpl implements UserValidationClientRepository {
 
@@ -29,8 +26,7 @@ public class UserValidationClientRepositoryImpl implements UserValidationClientR
 
     //user find by id
     @Override
-    public Optional<User> userValidateById(Long userId){
-        return jpaUserRepository.findById(userId)
-                .map(userPersistenceMapper::toDomainModel);
+    public boolean userValidateById(Long userId){
+        return jpaUserRepository.existsById(userId);
     }
 }
