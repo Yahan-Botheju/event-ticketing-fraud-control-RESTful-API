77bf35231638eefc42857689581a1f8ea60a58c7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-12 15:09:21 +0530

    fix: exception class typo issue

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/error_handling/exception/GlobalExceptionHandler.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/shared/error_handling/exception/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.e
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions.TicketAlreadyUsedException;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions.TicketReservedException;
-import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions.TicketTransferNotAllowedExecption;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions.TicketTransferNotAllowedException;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.DTOs.ApiResponse;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.DTOs.ErrorDetails;
 import org.apache.coyote.BadRequestException;
@@ -115,9 +115,9 @@ public class GlobalExceptionHandler {
     /* __DOMAIN_EXCEPTIONS__ */
 
     //409
-    @ExceptionHandler(TicketTransferNotAllowedExecption.class)
+    @ExceptionHandler(TicketTransferNotAllowedException.class)
     public ResponseEntity<ApiResponse<Void>> handleTicketSoldOutException(
-            TicketTransferNotAllowedExecption ex,
+            TicketTransferNotAllowedException ex,
             WebRequest request
     ){
         return buildResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
