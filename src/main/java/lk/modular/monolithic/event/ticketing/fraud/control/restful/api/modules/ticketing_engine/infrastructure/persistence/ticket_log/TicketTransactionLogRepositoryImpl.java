a4532d89e395972eaa2f03576ab3eb9cd32a7ad1
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-13 14:45:48 +0530

    feat: save logs in db

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket_log/TicketTransactionLogRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/infrastructure/persistence/ticket_log/TicketTransactionLogRepositoryImpl.java
@@ -1,6 +1,8 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket_log;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.records.TicketTransactionLog;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.TicketTransactionLogRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket_log.entities.TicketTransactionLogEntity;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket_log.jpa.JpaTicketTransactionLogRepository;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.infrastructure.persistence.ticket_log.persistenceMapper.TicketTransactionLogPersistenceMapper;
 
@@ -16,5 +18,13 @@ public class TicketTransactionLogRepositoryImpl implements TicketTransactionLogR
     ) {
         this.jpaTicketTransactionLogRepository = jpaTicketTransactionLogRepository;
         this.ticketTransactionLogPersistenceMapper = ticketTransactionLogPersistenceMapper;
+    }
+
+    @Override
+    public void save(TicketTransactionLog ticketTransactionLog) {
+        TicketTransactionLogEntity toEntity =
+                ticketTransactionLogPersistenceMapper.toEntity(ticketTransactionLog);
+
+        jpaTicketTransactionLogRepository.save(toEntity);
     }
 }
