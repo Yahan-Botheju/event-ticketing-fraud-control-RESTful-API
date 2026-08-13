910969693bbad7bacdc106ba369ed201f54101f0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-13 19:51:14 +0530

    ref: extends with domain exception handler and define error message

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/domain_exceptions/TicketAlreadyUsedException.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/domain_exceptions/TicketAlreadyUsedException.java
@@ -1,7 +1,7 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions;
 
-public class TicketAlreadyUsedException extends RuntimeException {
+public class TicketAlreadyUsedException extends DomainException {
     public TicketAlreadyUsedException(String message) {
-        super(message);
+        super("ERROR_TICKET_ALREADY_USED", message);
     }
 }
