d9da175b44fe8ee8d3d616b8cc92827280773006
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-13 14:10:22 +0530

    feat: initiate save ticket log method

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/TicketTransactionLogRepository.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/repositories/TicketTransactionLogRepository.java
@@ -1,4 +1,8 @@
 package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories;
 
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.records.TicketTransactionLog;
+
 public interface TicketTransactionLogRepository {
+    //save ticket transaction log
+    void save(TicketTransactionLog ticketTransactionLog);
 }
