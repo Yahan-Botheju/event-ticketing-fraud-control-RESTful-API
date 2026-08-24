30414dcd42634eafa6b151cc41307695ad3515b0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-24 19:43:22 +0530

    feat: make ticket domain model clean

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/models/Ticket.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/domain/models/Ticket.java
@@ -2,18 +2,12 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions.TicketAlreadyUsedException;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.domain_exceptions.TicketTransferNotAllowedException;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Getter
 public class Ticket {
     private Long ticketId;
     private String ticketCode;
@@ -27,6 +21,27 @@ public class Ticket {
 
     private LocalDateTime purchasedAt;
     private LocalDateTime scannedAt;
+
+    public Ticket(
+            Long ticketId,
+            String ticketCode,
+            Long eventId,
+            Long ownerId,
+            BigDecimal ticketPrice,
+            TicketStatus ticketStatus,
+            LocalDateTime purchasedAt,
+            LocalDateTime scannedAt
+    ) {
+        this.ticketId = ticketId;
+        this.ticketCode = ticketCode;
+        this.eventId = eventId;
+        this.ownerId = ownerId;
+        this.ticketPrice = ticketPrice;
+        this.ticketStatus = ticketStatus;
+        this.purchasedAt = purchasedAt;
+        this.scannedAt = scannedAt;
+    }
+
 
     /* __DOMAIN_BUSINESS_LOGICS__ */
 
