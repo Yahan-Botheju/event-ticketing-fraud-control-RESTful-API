51d204fb30dbef675ab17414cc662760f8fb7423
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-25 20:17:29 +0530

    ref: change method parameters and use method in required positions

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/register/RegisterUserUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/identity_security/usecase/register/RegisterUserUseCaseImpl.java
@@ -4,6 +4,7 @@ import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.i
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.models.User;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.IdentityProvider;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.domain.repositories.UserRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.identity_security.usecase._records.RegisterRequestCommand;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.exception.ConflictException;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,23 +26,23 @@ public class RegisterUserUseCaseImpl implements RegisterUserUseCase {
     //register user
     @Override
     @Transactional
-    public void register(User user) {
-        if(userRepository.existsByEmail(user.getEmail())) {
+    public void register(RegisterRequestCommand registerCommand) {
+        if(userRepository.existsByEmail(registerCommand.email())) {
             throw new ConflictException("Email already registered..!!");
         }
 
         //encode password using  identity domain repo
-        String encodedPassword = identityProvider.encodePassword(user.getPassword());
+        String encodedPassword = identityProvider.encodePassword(registerCommand.password());
 
         //if role is empty set as default role
-        Role userRole = user.getRole() != null ? user.getRole() : Role.ATTENDEE;
+        Role userRole = registerCommand.role() != null ? registerCommand.role() : Role.ATTENDEE;
 
         //create user object with required details
         User newUser = User.registerNewUser(
-                user.getFullName(),
-                user.getEmail(),
+                registerCommand.fullName(),
+                registerCommand.email(),
                 encodedPassword,
-                user.getRole());
+                registerCommand.role() == null ? userRole : registerCommand.role());
 
         //save user
         userRepository.registerUser(newUser);
