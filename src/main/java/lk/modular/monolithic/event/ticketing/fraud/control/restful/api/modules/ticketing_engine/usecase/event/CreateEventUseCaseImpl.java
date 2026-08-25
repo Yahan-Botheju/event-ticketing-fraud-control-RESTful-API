74868acc6ed45e967e03cf82d237dd21ac0c2aa3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-25 21:16:09 +0530

    ref: set required parameters and use them

--- src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/CreateEventUseCaseImpl.java
+++ src/main/java/lk/modular/monolithic/event/ticketing/fraud/control/restful/api/modules/ticketing_engine/usecase/event/CreateEventUseCaseImpl.java
@@ -2,6 +2,7 @@ package lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.
 
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.models.Event;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.domain.repositories.EventRepository;
+import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.modules.ticketing_engine.usecase._records.CreateEventRequestCommand;
 import lk.modular.monolithic.event.ticketing.fraud.control.restful.api.shared.error_handling.exception.InvalidTicketException;
 
 import java.math.BigDecimal;
@@ -21,19 +22,19 @@ public class CreateEventUseCaseImpl implements CreateEventUseCase {
 
     //create event
     @Override
-    public Event execute(Event event, Long organizerId) {
+    public Event execute(CreateEventRequestCommand requestCommand, Long organizerId) {
         //check ticket availability
-        if(event.getEventTotalTickets() == null || event.getEventTicketPrice().compareTo(BigDecimal.ZERO) <= 0){
+        if(requestCommand.eventTotalTickets() == null || requestCommand.eventTicketPrice().compareTo(BigDecimal.ZERO) <= 0){
             throw new InvalidTicketException("Total tickets must be greater than zero");
         }
         //set event available tickets to total tickets
         Event newEvent = Event.createNewEvent(
-                event.getEventTitle(),
-                event.getEventDescription(),
-                event.getEventLocation(),
-                event.getEventDate(),
-                event.getEventTotalTickets(),
-                event.getEventTicketPrice(),
+                requestCommand.eventTitle(),
+                requestCommand.eventDescription(),
+                requestCommand.eventLocation(),
+                requestCommand.eventDate(),
+                requestCommand.eventTotalTickets(),
+                requestCommand.eventTicketPrice(),
                 organizerId
         );
 
